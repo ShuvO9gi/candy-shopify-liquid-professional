c993c3c66afaa14a806b30013f5aae027074fff5
Md. Raju Ahmed <rajuahmad6789@gmail.com>
2024-05-14 14:25:51 +0600

    yarn watch for theme pull

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,4 +1,3 @@
-"use strict";
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
@@ -15,12 +14,15 @@
   \**************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var picoapp__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! picoapp */ \"./node_modules/picoapp/dist/picoapp.es.js\");\n\n// import frontpageTop from './js/components/frontpageTop';\n// import blandSelvSlik from './js/custom/blandSelvSlik';\n// import cart from './js/templates/cart';\n// import tutorialPopups from './js/components/tutorialPopups';\n// import collection from './js/templates/collection';\n// import product from './js/templates/product';\n// import customScrollbar from './js/components/customScrollbar';\n// import header from './js/templates/header';\n\n// require('./css/app.css');\n\n// const app = picoapp({\n//   frontpageTop,\n//   blandSelvSlik,\n//   cart,\n//   tutorialPopups,\n//   collection,\n//   product,\n//   customScrollbar,\n//   header,\n// });\n\nconst app = (0,picoapp__WEBPACK_IMPORTED_MODULE_0__.picoapp)();\napp.hydrate({\n  cart: theme.settings.cart,\n  cartTotal: theme.settings.cart.total_price\n});\n\n// window.app = app;\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (app);\n\n//# sourceURL=webpack:///./resources/app.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var picoapp__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! picoapp */ \"./node_modules/picoapp/dist/picoapp.es.js\");\n // import frontpageTop from './js/components/frontpageTop';\n// import blandSelvSlik from './js/custom/blandSelvSlik';\n// import cart from './js/templates/cart';\n// import tutorialPopups from './js/components/tutorialPopups';\n// import collection from './js/templates/collection';\n// import product from './js/templates/product';\n// import customScrollbar from './js/components/customScrollbar';\n// import header from './js/templates/header';\n// require('./css/app.css');\n// const app = picoapp({\n//   frontpageTop,\n//   blandSelvSlik,\n//   cart,\n//   tutorialPopups,\n//   collection,\n//   product,\n//   customScrollbar,\n//   header,\n// });\n\nconst app = (0,picoapp__WEBPACK_IMPORTED_MODULE_0__.picoapp)();\napp.hydrate({\n  cart: theme.settings.cart,\n  cartTotal: theme.settings.cart.total_price\n}); // window.app = app;\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (app);\n\n//# sourceURL=webpack:///./resources/app.js?");
 
 /***/ })
 
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
 /******/ var __webpack_exports__ = (__webpack_exec__("./resources/app.js"));
 /******/ }

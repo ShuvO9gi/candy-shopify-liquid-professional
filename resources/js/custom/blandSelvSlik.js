a2789655c81b368466b1e1e55f1c663c625f2f44
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-05-15 01:30:32 +0600

    filteredd products updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1099,6 +1099,13 @@ export default component((node, ctx) => {
 
       productListElem.innerHTML = filteredProducts;
 
+      // const filteredProducts = allFilters[0].activeTypes[0];
+
+      // productListElem.innerHTML = `
+      //       <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px;">${filteredProducts}</h2>
+      //       <div class="candyItems">${renderProducts(unique)}</div>
+      //   `;
+
       addToCardEventListeners();
     });
 

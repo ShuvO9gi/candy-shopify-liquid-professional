b76904a947081d5873126a677465ba032061503d
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-05-29 01:31:40 +0600

    products filtered updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1139,7 +1139,7 @@ export default component((node, ctx) => {
             if (filteredProducts.length > 0) {
               categorizedHTML += `
               <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px;">${showCategory}</h2>
-
+              <div class="candyItems">${renderProducts(filteredProducts)}</div>
           `;
             }
           });

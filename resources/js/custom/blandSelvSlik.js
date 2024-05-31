201d5ac25e5ad46d37710e30cd5b1f573a235118
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-05-31 18:27:36 +0600

    uncategorized products filtered updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1147,9 +1147,11 @@ export default component((node, ctx) => {
 
           if (uncategorizedProducts.length > 0) {
             categorizedHTML += `
-          <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px;">Other</h2>
-          <div class="candyItems">${renderProducts(uncategorizedProducts)}</div>
-        `;
+              <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px;">Other</h2>
+              <div class="candyItems">${renderProducts(
+                uncategorizedProducts
+              )}</div>
+            `;
           }
 
           return categorizedHTML;

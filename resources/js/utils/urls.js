3aa17af973fcb927ea2dc24e5473e91451133a01
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-06-18 00:22:08 +0600

    url question mark issue initiated

--- resources/js/utils/urls.js
+++ resources/js/utils/urls.js
@@ -1,20 +1,20 @@
 export function setUrlParams(parameters) {
-  let url = new URL(window.location.href)
-  let params = new URLSearchParams(url.search.slice(1))
-  
-  parameters.forEach(({ name, value }) => {
-      if (value != "") params.set(name, encodeURIComponent(value))
-      else params.delete(name)
-  })
+  let url = new URL(window.location.href);
+  let params = new URLSearchParams(url.search.slice(1));
 
-  window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`)
+  parameters.forEach(({ name, value }) => {
+    if (value != "") params.set(name, encodeURIComponent(value));
+    else params.delete(name);
+  });
+
+  //window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`)
 }
 
 export function deleteUrlParams(paramName) {
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
-  
-  params.delete(paramName);  
+
+  params.delete(paramName);
   url.search = params.toString();
   window.history.replaceState(null, null, url.href);
 }

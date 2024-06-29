9b5f7e4f4abe7bf276de5c047fee1a8205947a76
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-06-29 21:21:39 +0600

    ui updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1287,7 +1287,7 @@ export default component((node, ctx) => {
               .map(
                 (product) => `
               <div class="swiper-slide">
-                
+                ${renderProducts([product])}
               </div>
             `
               )

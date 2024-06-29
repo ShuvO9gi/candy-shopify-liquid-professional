5f0cf5b60a9f553a2c93048f0eb0bb71ca46cfb7
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-06-29 21:06:09 +0600

    ui updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1282,7 +1282,9 @@ export default component((node, ctx) => {
               filteredProductsHTML += `
                 <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px; text-wrap: nowrap;">${showTitle}</h2>
           <div class="swiper-container candyItems">
+            <div class="swiper-wrapper">
             
+            </div>
           </div>
           `;
             }

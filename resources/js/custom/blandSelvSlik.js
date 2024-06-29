067306fc30f955a1305877d318ac36cb10c2f998
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-06-29 21:05:59 +0600

    ui updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1280,9 +1280,11 @@ export default component((node, ctx) => {
             );
             if (filteredItems.length > 0) {
               filteredProductsHTML += `
-                <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px;">${showTitle}</h2>
-                <div class="candyItems">${renderProducts(filteredItems)}</div>
-              `;
+                <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px; text-wrap: nowrap;">${showTitle}</h2>
+          <div class="swiper-container candyItems">
+            
+          </div>
+          `;
             }
 
             return filteredProductsHTML;

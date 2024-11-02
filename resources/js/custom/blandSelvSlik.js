dd5c5301cd49ff5a4fa812f7f95f51c845155628
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-11-03 00:46:26 +0600

    swiper for mobileview updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1209,6 +1209,10 @@ export default component((node, ctx) => {
 
         if (filteredProducts.length > 0) {
           if (!isMobileView) {
+            categorizedHTML += `
+          <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px; text-wrap: nowrap;">${showCategory}</h2>
+          
+          `;
           } else {
             categorizedHTML += `
           <h2 class="font-bold text-3xl pl-4 capitalize" style="padding-bottom: 36px; text-wrap: nowrap;">${showCategory}</h2>

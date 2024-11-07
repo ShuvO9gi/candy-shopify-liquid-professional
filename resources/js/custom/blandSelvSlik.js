b08dc0cded6aea2e5f826d94afcd82f92592c156
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-11-07 22:22:50 +0600

    swiper functionality updated

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1212,6 +1212,17 @@ export default component((node, ctx) => {
             reachBeginning: function () {
               const wrapper = swiper.wrapperEl;
               const transform = window.getComputedStyle(wrapper).transform;
+
+              if (transform !== "none") {
+                const matrix = new WebKitCSSMatrix(transform);
+                const translateX = matrix.m41; // Get the current translateX value
+
+                // Adjust to ensure the first slide is fully visible
+                const slideWidth = swiper.slides[0].offsetWidth;
+                const newTranslateX = translateX - (swiper.width - slideWidth);
+
+                wrapper.style.transform = `translate3d(${newTranslateX}px, 0, 0)`;
+              }
             },
           },
         });

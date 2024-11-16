3005b3dbd0b267e4c4302a8feae09241f53e8634
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-11-16 23:49:27 +0600

    fix selected items filter

--- resources/js/custom/blandSelvSlik.js
+++ resources/js/custom/blandSelvSlik.js
@@ -1104,6 +1104,8 @@ export default component((node, ctx) => {
                 item.click();
               }
             });
+
+            categoryIds.push(param);
           }
         });
       };

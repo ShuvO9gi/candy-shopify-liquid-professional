396afdcc8a2d72455172027a2369d781d64f6c26
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-12-02 01:34:17 +0600

    category filter issue fixed

--- resources/js/components/cleanPose.js
+++ resources/js/components/cleanPose.js
@@ -1,11 +1,11 @@
-import { component } from 'picoapp';
-import setToLocalStorage from './setToLocaleStorage';
+import { component } from "picoapp";
+import setToLocalStorage from "./setToLocaleStorage";
 
 export default component((node, ctx) => {
-    node.addEventListener('click', (e) => {
-        // e.preventDefault();
-        // console.log('click on me')
-        setToLocalStorage(null, [])
-        ctx.emit('products:refetch')
-    });
-})
+  node.addEventListener("click", (e) => {
+    // e.preventDefault();
+    // console.log('click on me')
+    setToLocalStorage(null, []);
+    ctx.emit("products:refetch");
+  });
+});
